9f52b6dff844f9ae5c299524c547136ba14a05d3
parkhb0 <parkhyunbo@parkui-MacBookPro.local>
2023-12-30 22:56:11 +0900

    msw test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+
+import { worker } from "./mocks/worker";
 import App from "./App";
+if (process.env.NODE_ENV === "development") {
+  console.log("1");
+  worker.start();
+}
+
+const router = "";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <RouterProvider router={router} fallbackElement={<div>Loading...</div>} /> */}
   </React.StrictMode>
 );
